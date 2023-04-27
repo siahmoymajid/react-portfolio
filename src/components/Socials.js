4afb274ea627f2b6c7dd26dea4ac2bd0343e31ef
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-04-27 14:08:16 -0700

    added links for the contact

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,27 +1,43 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import { Github, Linkedin, Envelope } from 'react-bootstrap-icons';
+import { Container, Row, Col } from "react-bootstrap";
+import { Github, Linkedin, Envelope } from "react-bootstrap-icons";
 
 function Socials({ github, linkedin, email }) {
   return (
-      <Row className='d-flex justify-content-start align-items-center mt-auto mb-3'>
-        <Col>
-          <a className="icon-link link-dark" href={github} target='_blank' rel="noopener noreferrer" >
-            <Github />
-          </a>
-        </Col>
-        <Col>
-          <a className="icon-link link-dark" href={linkedin}  target='_blank' rel="noopener noreferrer" >
-            <Linkedin />
-          </a>
-        </Col>
-        <Col>
-          <a className="icon-link link-dark" href={`mailto:${email}`} target='_blank' rel="noopener noreferrer">
-            <Envelope />
-          </a>
-        </Col>
-        <Col className='ms-auto copyright text-nowrap'>Copyright &copy; Sia Majid</Col>
-      </Row>
-
+    <Row className="d-flex justify-content-start align-items-center mt-auto mb-3">
+      <Col>
+        <a
+          className="icon-link link-dark"
+          href={"https://github.com/siahmoymajid"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+      </Col>
+      <Col>
+        <a
+          className="icon-link link-dark"
+          href={"https://www.linkedin.com/in/siahmoymajid/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Linkedin />
+        </a>
+      </Col>
+      <Col>
+        <a
+          className="icon-link link-dark"
+          href={`mailto:${"smajid3@horizon.csueastbay.edu"}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Envelope />
+        </a>
+      </Col>
+      <Col className="ms-auto copyright text-nowrap">
+        Copyright &copy; Sia Majid
+      </Col>
+    </Row>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>kkjhhkl</div>
+    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta laborum eos deleniti, culpa incidunt fugiat dolor sit sunt, praesentium architecto minus quo dignissimos laboriosam sequi veritatis voluptatum accusamus voluptate sed.</div>
   )
 }
 

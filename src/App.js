3f0069914b722c80106e906ce3a95071571d406c
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-05-06 18:57:43 -0700

    fixed profile picture

--- src/App.js
+++ src/App.js
@@ -25,26 +25,44 @@ function App() {
   }, [])
 
   return (
-    data &&
-    <BrowserRouter>
-      <Container fluid>
-        <Row className="d-flex justify-content-center align-items-center w-100 my-5">
-          <Col xs={12} lg={10} className="px-5 py-2 border-5 rounded-4 bg-dark d-flex flex-column justify-content-center align-items-center text-light">
-            <Header name={name} role={role} />
-            <Routes>
-             
-              <Route path="projects" element={<Projects pageTitle={portfolio} projects={projects} />} />
-              <Route path="contact" element={<Contact data={contact} title={contact} />} />
-              <Route path="resume" element={<Resume data={resume} skills={skills} title={resume} />} />
-              <Route path="/" element={<About title={about} bio={bio} name={name} role={role} picture={picture}/>} />
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-            <Footer github={github} linkedin={linkedin} email={email} />
-          </Col>
-        </Row>
-      </Container>
-    </BrowserRouter>
-
+    data && (
+      <BrowserRouter>
+        <Container fluid>
+          <Row className="d-flex justify-content-center align-items-center w-100 my-5">
+            <Col
+              xs={12}
+              lg={10}
+              className="px-5 py-2 border-5 rounded-4 bg-dark d-flex flex-column justify-content-center align-items-center text-light"
+            >
+              <Header name={name} role={role} />
+              <Routes>
+                <Route
+                  path="projects"
+                  element={
+                    <Projects pageTitle={portfolio} projects={projects} />
+                  }
+                />
+                <Route
+                  path="contact"
+                  element={<Contact data={contact} title={contact} />}
+                />
+                <Route
+                  path="resume"
+                  element={
+                    <Resume data={resume} skills={skills} title={resume} />
+                  }
+                />
+                
+                
+               
+              </Routes>
+              <About title={about} bio={bio} name={name} role={role} />
+              <Footer github={github} linkedin={linkedin} email={email} />
+            </Col>
+          </Row>
+        </Container>
+      </BrowserRouter>
+    )
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Image, Row } from 'react-bootstrap';
 import Heading from '../components/Heading';
+import profilePicture from "../public/assets/images/profile.jpg"
 
 function About({ name, bio, role, picture }) {
 
@@ -8,7 +9,7 @@ function About({ name, bio, role, picture }) {
     <>
       <Row>
         <Col lg={6} xl={4}>
-          <Image src={`/assets/images/${picture}`} width={400} height={400} className="shadow-50 rounded-circle overflow-hidden align-self-start justify-self-center d-block border-4 bg-dark p-2" alt={name} />
+          <Image src={profilePicture} width={400} height={400} className="shadow-50 rounded-circle overflow-hidden align-self-start justify-self-center d-block border-4 bg-dark p-2" alt={name} />
         </Col>
         <Col md={8}>
           <Heading name={name} role={role && role} />
